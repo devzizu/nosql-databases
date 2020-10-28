c3a7607e1e005f5bf9d6bc459b64cd3f5c6ef58a
devzizu <jazevedo960@gmail.com>
2020-10-28 15:44:34 +0000

    fixing paths v2

--- scripts/editora.sql
+++
@@ -1,5 +0,0 @@
-insert into editora values (1, 'EMI');
-insert into editora values (2, 'Sony');
-insert into editora values (3, 'Valentim de Carvalho');
-insert into editora values (4, 'Vevo');
-insert into editora values (5, 'Apple');
