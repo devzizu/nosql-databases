2417a4cbb2041df9b9a003c596baed67f05c451a
devzizu <jazevedo960@gmail.com>
2020-11-18 10:24:53 +0000

    finished ficha 3

--- fe03/scripts/config.py
+++ fe03/scripts/config.py
@@ -1,4 +1,5 @@
 base_api_url = 'http://nosql.hpeixoto.me/api/'
 blood_api   = 'sensor/400'
+cardiac_api = 'sensor/300'
 
 mongodb_uri = "mongodb://localhost:27017"
--- fe03/scripts/main.py
+++ fe03/scripts/main.py
@@ -8,83 +8,51 @@ import config as cfg
 from pymongo import MongoClient
 from pprint import pprint
 
-REQUESTS = {}
-time_sleep = 3
+time_sleep = 6
 
 def main():
 
     os.system('cls' if os.name == 'nt' else 'clear')
     print("[SCRIPT] Starting...")
 
-    load_api_to_dict()
+    cardiacAPI = load_api_to_dict(cfg.cardiac_api)
+    bloodAPI   = load_api_to_dict(cfg.blood_api)
 
     connect_mongodb()
     global DB
     DB = MDB_CLIENT.ficha3
 
     # create/switch to collection
-    bloodCollection = DB.blood
-    
-    # clear all documents
-    bloodCollection.delete_many({})
+    sensorCollection = DB.sensor
 
-    populate_collection(bloodCollection)
+    # clear all documents
+    sensorCollection.delete_many({})
+
+    populate_collection(sensorCollection, cardiacAPI)
+    populate_collection(sensorCollection, bloodAPI)
+    
 
     while True:
         print("[SCRIPT] sleeping... (+", time_sleep,"s)")
         time.sleep(time_sleep)
-        update_collection(bloodCollection)
+
+        cardiacAPI = load_api_to_dict(cfg.cardiac_api)
+        bloodAPI   = load_api_to_dict(cfg.blood_api)
+
+        populate_collection(sensorCollection, cardiacAPI)
+        populate_collection(sensorCollection, bloodAPI)
 
     print("[SCRIPT] Finished!")
 
-def update_collection(collection):
-    
-    print("[COLLECTION] updating...")
-
-    load_api_to_dict()
-
-    for req in REQUESTS:
-        request = REQUESTS[req]
-        medidaNova = {}
-        medidaNova["timestamp"] = request["timestamp"]
-        medidaNova["bodytemp"] = request["bodytemp"]
-        medidaNova["bloodpress_systolic"] = request["bloodpress"]["systolic"]
-        medidaNova["bloodpress_diastolic"] = request["bloodpress"]["diastolic"]
-        medidaNova["bpm"] = request["bpm"]
-        medidaNova["sato2"] = request["sato2"]
-        
-        collection.update_one({"sensorid": request["sensorid"]}, {"$push": { "medida": medidaNova}})
-
-    print("[COLLECTION] done update!")
-
-def populate_collection(collection):
+def populate_collection(collection, reqs):
 
     print("[COLLECTION] populating...")
 
     medida = {}
 
-    for req in REQUESTS:
-        
-        request = REQUESTS[req]
-        medida["timestamp"] = request["timestamp"]
-        medida["bodytemp"] = request["bodytemp"]
-        medida["bloodpress_systolic"] = request["bloodpress"]["systolic"]
-        medida["bloodpress_diastolic"] = request["bloodpress"]["diastolic"]
-        medida["bpm"] = request["bpm"]
-        medida["sato2"] = request["sato2"]
-    
-        sensor_req = request
-        del sensor_req["timestamp"]
-        del sensor_req["bodytemp"]
-        del sensor_req["bpm"]
-        del sensor_req["sato2"]
-        del sensor_req["bloodpress"]["systolic"]
-        del sensor_req["bloodpress"]["diastolic"]
-        del sensor_req["bloodpress"]
-
-        sensor_req["medida"] = [medida]
-        
-        collection.insert_one(sensor_req)
+    for req in reqs:
+        request = reqs[req]
+        collection.insert_one(request)
  
     print("[COLLECTION] done populate!")
 
@@ -92,20 +60,24 @@ def connect_mongodb():
     global MDB_CLIENT
     MDB_CLIENT = MongoClient(cfg.mongodb_uri)
 
-def load_api_to_dict():
+def load_api_to_dict(api):
 
-    print("[API] Loading {} data...".format(cfg.base_api_url))
+    REQUESTS = {}
+
+    print("[API] Loading {} data... ({})".format(cfg.base_api_url, api))
     
     range_min, range_max = 1, 6
     for req in range(range_min, range_max):
         url         = '{}{}{}'.format(cfg.base_api_url, 
-                                      cfg.blood_api, req)
+                                      api, req)
         request     = requests.get(url)
         jsonRequest = request.json()
         jsonString  = json.dumps(jsonRequest)
         jsonData    = json.loads(jsonString)
         REQUESTS[req] = jsonData
     
+    return REQUESTS
+
     print("[API] Loaded!")
 
 if __name__ == '__main__':
